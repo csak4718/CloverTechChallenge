b899215fd65efd6a35bdb1c48b27a9997b765e9c
csak4718 <csak4718@yahoo.com.tw>
2016-03-15 11:14:59 -0500

    update

--- app/src/main/java/clover/superhero/MainActivity.java
+++ app/src/main/java/clover/superhero/MainActivity.java
@@ -2,6 +2,12 @@ package clover.superhero;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String requestUrl = "http://www.omdbapi.com/?s=";
+
+        List<String> res = new ArrayList<>();
+        ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.activity_main_list_view, res);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+        
+        String[] input = new String[] {"Batman", "Superman", "Fantastic Four"};
+        for (String str: input) {
+            str = str.replace(' ', '+');
+            Log.d("TEST", str);
+        }
+
+
     }
 }
